716d06ac7347bfafc962dfb1e3282decc0ff9c3c
jevvilla <juan.es-19@hotmail.com>
2019-11-18 06:12:34 -0500

    welcome screen created and added to the barrel to be used easily

---
+++ src/screens/Welcome/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+import colors from '../../common/colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,5 @@
 import Forgot from './Forgot';
 import Login from './Login';
+import Welcome from './Welcome';
 
-export { Forgot, Login };
+export { Forgot, Login, Welcome };
