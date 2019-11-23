d725b0fa47669333a0b626a1d15d066bd05ca879
jevvilla <juan.es-19@hotmail.com>
2019-11-23 17:51:33 -0500

    Welcome screen implemented

--- src/screens/Welcome/index.jsx
+++ src/screens/Welcome/index.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 
-import styles from './styles';
+import { Typography, Block, Button } from '../../common/components';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text>Welcome</Text>
-  </View>
-);
+import { sizes } from '../../common/styles';
+
+const Welcome = () => {
+  return (
+    <Block>
+      <Block center bottom flex={0.4}>
+        <Typography h1 center bold>
+          Your Home.
+          <Typography h1 primary>
+            {' '}
+            Greener.
+          </Typography>
+        </Typography>
+        <Typography h3 gray2 style={{ marginTop: sizes.padding / 2 }}>
+          Enjoy the experience.
+        </Typography>
+      </Block>
+      <Block center middle>
+        {Welcome.renderIllustrations()}
+        {Welcome.renderSteps()}
+      </Block>
+      <Block middle flex={0.5} margin={[0, sizes.padding * 2]}>
+        <Button gradient opacity={0.5}>
+          <Typography center semibold white>
+            Login
+          </Typography>
+        </Button>
+        <Button shadow opacity={0.5}>
+          <Typography center>Signup</Typography>
+        </Button>
+        <Button>
+          <Typography center>Terms of service</Typography>
+        </Button>
+      </Block>
+    </Block>
+  );
+};
+
+Welcome.navigationOptions = {
+  header: null,
+};
+
+Welcome.renderIllustrations = () => {
+  return (
+    <Block>
+      <Typography>Image</Typography>
+    </Block>
+  );
+};
+
+Welcome.renderSteps = () => {
+  return (
+    <Block>
+      <Typography>* * *</Typography>
+    </Block>
+  );
+};
 
 export default Welcome;
